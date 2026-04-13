c1a5052a57d9705d55b6087ffb9db672e894345b
uli lab <ulirizki@gmail.com>
2026-04-13 10:28:10 +0700

    materi 2  - migration : konfigurasi .env, create, dan rollback

---
+++ database/migrations/2026_04_13_024113_create_matakuliah_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('matakuliah', function (Blueprint $table) {
+            $table->id('matakuliah_id');
+            $table->char('kode_mk', 6);
+            $table->string('nama_mk');
+            $table->integer('sks');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('matakuliah');
+    }
+};
